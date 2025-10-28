d1c7b6c633798ced219b75afad8d8c1d1bcc1f28
AndersonBlanco <andersonblanco913@gmail.com>
2025-10-28 17:29:19 -0400

    node graph implementation

--- ktp-proyecto/src/App.tsx
+++ ktp-proyecto/src/App.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+//import React from "react";
 import Projects from "./components/Projects";
-import { read } from "./components/supabase/functions";
+//import { read } from "./components/supabase/functions";
 import "./styles.scss";
-
+import {GraphCanvas} from "reagraph"
 function App() {
   // will implemnent later
   //const filter = ["Computer Science", "Data Science", "Business", "Research"];
 
+
+
   return (
     <div className="App">
-      <Projects />
+       <h1>Hello Universe</h1>
 
+<div>
+       <GraphCanvas 
+        
+      nodes={[
+      {
+        id: 'n-1',
+        label: '1'
+      },
+      {
+        id: 'n-2',
+        label: '2'
+      }
+    ]}
+    edges={[
+      {
+        id: '1->2',
+        source: 'n-1',
+        target: 'n-2',
+        label: 'Edge 1-2'
+      }
+    ]}
+       />
     </div>
+    </div>
+  
   );
 }
 
